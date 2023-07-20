44db31ca476fe0f5c6e7cf430e55297c2a3cd277
Adit Bhosle <aditbhosle81@gmail.com>
2023-07-20 18:32:47 -0400

    Modifying script files to troubleshoot deployment issues

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,7 @@
 
 # Activate the virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
+cd /deployment/
 
 # Install the required Python packages
-pip install -r ../requirements.txt
+pip install -r requirements.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,7 @@
 
 # Activate the virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
+cd /deployment/
 
 # Start the Flask server
 python app.py
