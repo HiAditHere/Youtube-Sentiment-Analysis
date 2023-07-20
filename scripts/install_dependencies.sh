923691fbbe84fcb06f0c1711fcaf6cf099b99474
Adit Bhosle <aditbhosle81@gmail.com>
2023-07-20 00:57:31 -0400

    Modify install_dependencies.sh to troubleshoot deployment issue

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,7 @@
 
 # Activate the virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
+cd /var/www/html
 
 # Install the required Python packages
-pip install -r /var/www/html/requirements.txt
+pip install -r requirements.txt
