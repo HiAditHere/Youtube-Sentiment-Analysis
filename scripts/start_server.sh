ac6f29566294804769d410e55ad8080cd6ef6c79
Adit Bhosle <aditbhosle81@gmail.com>
2023-07-20 20:21:47 -0400

    Modifying yml and script files to troubleshoot deployment issue

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,7 @@ set -e
 
 # Activate the virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
-cd ../var/www/html/myapp
+cd ../var/www/html/
 
 # Start the Flask server
 python app.py
