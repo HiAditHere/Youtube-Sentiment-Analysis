d2adbeb9412dbadde2f3d474e4828cdb38aa6b47
Adit Bhosle <aditbhosle81@gmail.com>
2023-07-20 21:15:19 -0400

    Troubleshooting

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,6 @@ set -e
 
 # Activate the virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
-ls
 
 # Install the required Python packages
 pip install -r requirements.txt
