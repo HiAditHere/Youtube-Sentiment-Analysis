f4ca454af82e63bd807bd55091f833481ddc45ca
Adit Bhosle <aditbhosle81@gmail.com>
2023-07-20 20:31:03 -0400

    Modifying yml and script files to troubleshoot deployment issue

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,6 @@ set -e
 
 # Activate the virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
-cd /var/www/html/
 
 # Install the required Python packages
 pip install -r requirements.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,6 @@ set -e
 
 # Activate the virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
-cd /var/www/html/
 
 # Start the Flask server
 python app.py
