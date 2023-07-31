e17fe039f7b1165e70abbe6818e8fed9cf041696
Adit Bhosle <aditbhosle81@gmail.com>
2023-07-31 09:30:00 -0400

    Trobleshooting deployment issues

--- scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Activate the virtual environment (if applicable)
-# source /path/to/your/virtualenv/bin/activate
-ls
-
-# Install the required Python packages
-pip install -r requirements.txt
--- scripts/start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Activate the virtual environment (if applicable)
-# source /path/to/your/virtualenv/bin/activate
-
-# Start the Flask server
-python app.py
