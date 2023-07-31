7812b4159615ed52db5d8dfe04a9f568ce7031cf
Adit Bhosle <aditbhosle81@gmail.com>
2023-07-31 09:34:41 -0400

    Trobleshooting deployment issues

---
+++ scripts/bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Replace 'your-ec2-public-ip' with the actual public IP address of your EC2 instance
+EC2_PUBLIC_IP='18.206.161.186'
+
+# SSH command to check if there are any containers running
+containers_running=$(ssh -o "StrictHostKeyChecking=no" ubuntu@$EC2_PUBLIC_IP 'docker ps -q')
+
+if [ -n "$containers_running" ]; then
+  # If there are running containers, stop and remove them
+  ssh -o "StrictHostKeyChecking=no" ubuntu@$EC2_PUBLIC_IP 'docker stop $(docker ps -q) && docker rm $(docker ps -aq)'
+else
+  echo "No running containers found on the remote EC2 instance."
+fi
